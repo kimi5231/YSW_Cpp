59348643ecb6616d1559aa61dc827e34bec122f1
kimi5231 <djwjod@naver.com>
2024-02-25 02:09:03 +0900

    10-1-3 문제 풀이
    +10-1 문제 합본 업데이트

--- YSW_Cpp/YSW_Cpp/10-1-3.cpp
+++ YSW_Cpp/YSW_Cpp/10-1-3.cpp
@@ -13,28 +13,9 @@ public:
 	{
 		cout << "[" << xpos << ", " << ypos << "]" << endl;
 	}
-	Point& operator+=(const Point& ref)
-	{
-		xpos += ref.xpos;
-		ypos += ref.ypos;
-		return *this;
-	}
-	Point& operator-=(const Point& ref)
-	{
-		xpos -= ref.xpos;
-		ypos -= ref.ypos;
-		return *this;
-	}
-	friend Point operator-(const Point& pos1, const Point& pos2);
 	friend bool operator==(const Point& pos1, const Point& pos2);
 	friend bool operator!=(const Point& pos1, const Point& pos2);
 };
-
-Point operator-(const Point& pos1, const Point& pos2)
-{
-	Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
-	return pos;
-}
 
 bool operator==(const Point& pos1, const Point& pos2)
 {
@@ -56,25 +37,10 @@ int main(void)
 {
 	Point pos1(3, 4);
 	Point pos2(10, 20);
-	Point pos3 = pos1 - pos2;
-
-	pos1.ShowPosition();
-	pos2.ShowPosition();
-	pos3.ShowPosition();
-	cout << endl;
-
-	pos1 += pos2;
-	pos1.ShowPosition();
-	pos2.ShowPosition();
-	pos1 -= pos2;
-	pos1.ShowPosition();
-	pos2.ShowPosition();
-	cout << endl;
 
 	pos1.ShowPosition();
 	pos2.ShowPosition();
 	cout << (pos1 == pos2) << endl;
 	cout << (pos1 != pos2) << endl;
-
 	return 0;
 }
